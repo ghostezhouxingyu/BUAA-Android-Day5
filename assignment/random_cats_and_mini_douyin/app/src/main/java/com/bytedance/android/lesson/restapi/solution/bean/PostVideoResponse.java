5fb2284258ed9197fa551f001411a282a80448b0
ghostezhouxingyu <1173629761@qq.com>
2019-07-04 21:53:08 +0800

    the fifth homework

---
+++ assignment/random_cats_and_mini_douyin/app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/PostVideoResponse.java
@@ -0,0 +1,45 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.18 17:53
+ */
+public class PostVideoResponse {
+
+    // TO.DO-C2 (3) Implement your PostVideoResponse Bean here according to the response json
+
+    @SerializedName("result")
+    private Object result;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("success")
+    private boolean success;
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
